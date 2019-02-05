bb552146eeee8dd564efc53d4add73e735504255
jhallsea <jake.hall88@gmail.com>
2019-02-05 14:54:25 -0800

    Added implementation for fixed values, broke validation?

--- Sudoku/SudokuController.cs
+++ Sudoku/SudokuController.cs
@@ -56,7 +56,13 @@ namespace Sudoku
                     {
                         SudokuButton addButton = new SudokuButton(i, k, j, buildState.getVals()[i, j, k]);
                         addButton.Dock = DockStyle.Fill;
-                        addButton.Click += new EventHandler(ButtonClick);
+                        if(!buildState.getFixed()[i,j,k])
+                            addButton.Click += new EventHandler(ButtonClick);
+                        else{
+                            addButton.ForeColor = Color.White;
+                            addButton.BackColor = Color.Black;
+                        }
+                            
                         if (buildState.getVals()[i, k, j] > 0)
                             addButton.Text = buildState.getVals()[i, k, j].ToString();
                         subRegions[i].Controls.Add(addButton, k, j);
